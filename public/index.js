30e60ae883ddbd20a446125958fd5d27afdf6245
FL0RENCE <florence.lelaidier@devinci.fr>
2018-01-20 18:13:26 +0100

    step 1
    step 1

--- public/index.js
+++ public/index.js
@@ -12,16 +12,10 @@ function setPrice(deliveries){
 	 return deliveries.map(delivery => {
         const trucker=getTrucker(delivery.truckerId);
         const price=(trucker.pricePerKm*delivery.distance + trucker.pricePerVolume*delivery.volume);
-        const commission={
-            insurance: price*0.15,
-            treasury: delivery.distance%500+1,
-            convargo: price*0.15-delivery.commission.insurance
-        }
 
         return {
             ...delivery,
-            price,
-            commission
+            price
         }
     })
 }
